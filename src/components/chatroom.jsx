fccf1a64cf4eb61ca37ba24bc3c38b614bd1a187
tusharrao198 <tushar.rao198@gmail.com>
2021-04-09 18:17:47 +0530

    fixed message recieved in chat

--- src/components/chatroom.jsx
+++ src/components/chatroom.jsx
@@ -1,46 +1,39 @@
-import React, { Component } from 'react';
-import { Widget, addResponseMessage, addUserMessage } from 'react-chat-widget';
-import 'react-chat-widget/lib/styles.css';
+import React, { Component } from "react";
+import { Widget, addResponseMessage, addUserMessage } from "react-chat-widget";
+import "react-chat-widget/lib/styles.css";
 // import logo from './logo.svg';
 
 class ChatRoom extends Component {
-    state = {
-        messages: [],
+  state = {
+    messages: [],
+  };
+
+  componentDidMount() {
+    // addResponseMessage("Welcome to Audiocave!");
+  }
+
+  handleNewUserMessage = async (newMessage) => {
+    // this.setState({ messages: [...this.state.messages, newMessage]});
+    console.log(newMessage);
+    // addUserMessage("sa")
+    this.props.handleNewUserMessage(newMessage);
+    console.log("messages = ", this.state.messages);
+  };
+
+  render() {
+    if (this.props.newmessage) {
+      addResponseMessage(this.props.newmessage);
     }
-
-    componentDidMount() {
-        // addResponseMessage("Welcome to Audiocave!");
-
-    }
-
-    handleNewUserMessage = async(newMessage) => {
-        // this.setState({ messages: [...this.state.messages, newMessage]});
-        this.props.handleNewUserMessage(newMessage);
-        console.log("messages = ", this.state.messages);
-    };
-
-    // addUserMessage = async() => {
-    //     console.log("ADD USER MESSAGE", this.props.this.props.newmessageRecieved);
-    //     addUserMessage(this.props.newmessageRecieved);
-    //     // this.setState({ messages: [...this.state.messages, this.props.newmessageRecieved]});
-    //     console.log("messages in addUserMessage = ", this.state.messages);
-    // }
-
-    render() {
-        return (
-            <div className="App">
-                <Widget
-                handleNewUserMessage={this.handleNewUserMessage}
-                // addUserMessage={this.addUserMessage}
-                autofocus={false}
-                // title="My new awesome title"
-                // subtitle="And my cool subtitle"
-                />
-                {/* {
-                    this.props.newmessageRecieved? this.addUserMessage(this.props.newmessageRecieved) : <h1></h1>
-                } */}
-            </div>
-        );
-    } 
+    return (
+      <div className="App">
+        <Widget
+          handleNewUserMessage={this.handleNewUserMessage}
+          autofocus={true}
+          title="Audiocave Chat"
+          subtitle="Chat with your friends!"
+        />
+      </div>
+    );
+  }
 }
 export default ChatRoom;
